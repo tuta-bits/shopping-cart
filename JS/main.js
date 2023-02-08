8256fa680728d20fbdb29bef3136d09887a8e35f
tuta-bits <tuta@angelic.com>
2023-02-08 11:10:27 +1000

    Update Add more JS logics to main.js to make the website more responsive Add onclick function to index.html to toggle the menu option for mobile design Add styling to style.css to complement the changes Add styling for different screen sizes

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,135 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
     cart.classList.remove('active');
 };
+
+// For Cart smooth functioning
+if (document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}
+else {
+    ready();
+}
+
+// creating ready function
+function ready() {
+    // remove items from cart
+    var removeCartButtons = document.getElementsByClassName('cart-remove');
+    console.log(removeCartButtons);
+    for (var i = 0; i < removeCartButtons.length; i++) {
+        var button = removeCartButtons[i];
+        button.addEventListener('click', removeCartItem);
+    }
+
+    // Quantity changes
+    var quantityInputs = document.getElementsByClassName('cart-quantity');
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged);
+    }
+
+    // Add to cart
+    var addCart = document.getElementsByClassName('add-cart');
+    for (var i = 0; i < addCart.length; i++) {
+        var button = addCart[i];
+        button.addEventListener('click', addCartClicked);
+    }
+
+    // Checkout function
+    document.getElementsByClassName('btn-buy')[0].addEventListener('click', checkoutButtonClicked);
+
+}
+
+// Checkout Button Function
+function checkoutButtonClicked() {
+    alert('Your order has been placed \n!!! Thanks !!!');
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    while (cartContent.hasChildNodes()) {
+        cartContent.removeChild(cartContent.firstChild);
+    }
+    updatetotal();
+}
+
+//removeCartItem function
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updatetotal();
+}
+
+// Quantity Changed
+function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+
+    updatetotal();
+}
+
+//Add addCartClicked function
+function addCartClicked(event) {
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
+    var price = shopProducts.getElementsByClassName('price')[0].innerText;
+    var prodcutImg = shopProducts.getElementsByClassName('product-img')[0].src;
+    addProductToCart(title, price, prodcutImg);
+    updatetotal();
+}
+
+// addProductToCart function
+function addProductToCart(title, price, prodcutImg) {
+    var cartShopBox = document.createElement('div');
+    cartShopBox.classList.add('cart-box');
+    var cartItems = document.getElementsByClassName('cart-content')[0];
+    var cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
+    for (var i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText == title) {
+            alert("You've already added this item to cart");
+
+            return;
+        }
+               
+    }
+
+    //Swapping dispalyed items into the cart items
+    var cartBoxContent = `
+        <img src="${prodcutImg}" alt="Sneakers" class="cart-img">
+        <div class="detail-box">
+            <div class="cart-product-title">${title}</div>
+            <div class="cart-price">${price}</div>
+            <input type="number" value="1" class="cart-quantity">
+        </div>
+        <!-- Remove Cart -->
+        <i class="bx bx-trash cart-remove"></i>`;
+
+    
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
+cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);
+
+}
+
+
+
+//Update total
+function updatetotal() {
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    var cartBoxes = cartContent.getElementsByClassName('cart-box');
+    var total = 0;
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        var price = parseFloat(priceElement.innerText.replace('$', ''));
+        var quantity = quantityElement.value;
+        total = total + price * quantity;
+        }
+
+        // rounding decimal prices
+        total = Math.round(total * 100) / 100;
+
+        document.getElementsByClassName('total-price')[0].innerText = '$' + total;
+    
+}
