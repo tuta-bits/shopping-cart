6cdbf985f39fa628ebe88ae380f6588ee83c0b88
tuta-bits <tuta@angelic.com>
2023-07-05 16:52:06 +1000

    Modifications Add another section in the DOM to display image Add section to display more information on the product selected Add JS codes to make the new section responsive Add more css for the new section Add light-Box fucntionality Add new style folder for css files

--- JS/main.js
+++ JS/main.js
@@ -22,34 +22,41 @@ else {
     ready();
 }
 
-// creating ready function
 function ready() {
+    loadContent();
+}
+
+// creating ready function
+function loadContent() {
     // remove items from cart
-    var removeCartButtons = document.getElementsByClassName('cart-remove');
-    console.log(removeCartButtons);
-    for (var i = 0; i < removeCartButtons.length; i++) {
-        var button = removeCartButtons[i];
+    let removeCartButtons = document.querySelectorAll('.cart-remove');
+    removeCartButtons.forEach((button)=>{
         button.addEventListener('click', removeCartItem);
-    }
+    });
+
+
 
     // Quantity changes
-    var quantityInputs = document.getElementsByClassName('cart-quantity');
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i];
+    let quantityInputs = document.querySelectorAll('.cart-quantity');
+    quantityInputs.forEach((input)=>{
         input.addEventListener('change', quantityChanged);
-    }
+    });
 
     // Add to cart
-    var addCart = document.getElementsByClassName('add-cart');
-    for (var i = 0; i < addCart.length; i++) {
-        var button = addCart[i];
+    let addCart = document.querySelectorAll('.add-cart');
+    addCart.forEach((button)=>{
         button.addEventListener('click', addCartClicked);
-    }
+    });
 
-    // Checkout function
-    document.getElementsByClassName('btn-buy')[0].addEventListener('click', checkoutButtonClicked);
+ 
+      // Checkout function
+      document.getElementsByClassName('btn-buy')[0].addEventListener('click', checkoutButtonClicked);
 
+
+   updateTotal();
 }
+
+
 
 // Checkout Button Function
 function checkoutButtonClicked() {
@@ -58,90 +65,209 @@ function checkoutButtonClicked() {
     while (cartContent.hasChildNodes()) {
         cartContent.removeChild(cartContent.firstChild);
     }
-    updatetotal();
+
+    loadContent();
+    
 }
 
 //removeCartItem function
-function removeCartItem(event) {
-    var buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
-    updatetotal();
-}
+function removeCartItem() {
+    let title = this.parentElement.querySelector('.cart-product-title').innerHTML;
+    itemList = itemList.filter(el => el.title != title);
+    this.parentElement.remove();
+    loadContent();
+} 
+
 
 // Quantity Changed
-function quantityChanged(event) {
-    var input = event.target;
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1;
+function quantityChanged() {
+    if (isNaN(this.value) || this.value <= 0) {
+        this.value = 1;
     }
 
-    updatetotal();
+    loadContent();
 }
+
+let itemList = [];
 
 //Add addCartClicked function
-function addCartClicked(event) {
-    var button = event.target;
-    var shopProducts = button.parentElement;
-    var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
-    var price = shopProducts.getElementsByClassName('price')[0].innerText;
-    var prodcutImg = shopProducts.getElementsByClassName('product-img')[0].src;
-    addProductToCart(title, price, prodcutImg);
-    updatetotal();
-}
+function addCartClicked() {
+    let shopProducts = this.parentElement;
+    let title = shopProducts.querySelector('.product-title').innerHTML;
+    let price = shopProducts.querySelector('.price').innerHTML;
+    let productImg = shopProducts.querySelector('.product-img').src;
+    //console.log(title, price, prodcutImg);
 
-// addProductToCart function
-function addProductToCart(title, price, prodcutImg) {
-    var cartShopBox = document.createElement('div');
-    cartShopBox.classList.add('cart-box');
-    var cartItems = document.getElementsByClassName('cart-content')[0];
-    var cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
-    for (var i = 0; i < cartItemsNames.length; i++) {
-        if (cartItemsNames[i].innerText == title) {
-            alert("You've already added this item to cart");
+    let newProduct = {title, price, productImg}
 
-            return;
-        }
-               
+    //Check product alreday exist in cart
+    if (itemList.find((el) => el.title == newProduct.title)) {
+        alert("You've already added this item to cart");
+        return;
+    }
+    else {
+        itemList.push(newProduct);
     }
 
-    //Swapping dispalyed items into the cart items
-    var cartBoxContent = `
-        <img src="${prodcutImg}" alt="Sneakers" class="cart-img">
+    // Adding products to cart
+    let newProductElem = addProductToCart(title, price, productImg);
+    let cartShopBox = document.createElement('div');
+    cartShopBox.innerHTML = newProductElem;
+    let cartItems = document.querySelector('.cart-content');
+    cartItems.append(cartShopBox);
+    loadContent();
+
+}
+
+
+
+
+// Add product to cart function
+
+function addProductToCart(title, price, productImg) {
+    return `
+    <div class="cart-box">
+        <img src="${productImg}"class="cart-img">
         <div class="detail-box">
             <div class="cart-product-title">${title}</div>
             <div class="cart-price">${price}</div>
             <input type="number" value="1" class="cart-quantity">
         </div>
-        <!-- Remove Cart -->
-        <i class="bx bx-trash cart-remove"></i>`;
+        <i class="bx bx-trash cart-remove"></i>
+    </div>  
+    `;
+}
 
     
-cartShopBox.innerHTML = cartBoxContent;
-cartItems.append(cartShopBox);
-cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
-cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);
 
-}
+
+
+
+
+const myCart = [];
+//-------------------------
+       // Add item to cart
+       function addItem() {
+        
+        let title = document.querySelector('.got').innerHTML;
+        let price = document.querySelector('.rap').innerHTML;
+        let prodImg = document.querySelector('.here').src;
+        console.log(title, price, prodImg);
+
+
+        let newProd = {title, price, prodImg};
+
+        if (myCart.find((el) => el.prodImg == newProd.prodImg)) {
+            alert("You've already added this item to cart");
+            return;
+        }
+        else {
+            myCart.push(newProd);
+        }
+
+        //-------------------------
+        //Add product to cart
+
+        let prodElem = addProd(title, price, prodImg);
+        let cartBox = document.createElement('div');
+        cartBox.innerHTML = prodElem;
+        let cartItem = document.querySelector('.cart-content');
+        cartItem.append(cartBox);
+        loadContent();
+
+      }
+
+      //--------------------
+      // Display the product in the cart
+      
+      function addProd(title, price, prodImg) {
+        return `
+        <div class="cart-box">
+        <img src="${prodImg}"class="cart-img">
+        <div class="detail-box">
+            <div class="cart-product-title">${title}</div>
+            <div class="cart-price">${price}</div>
+            <input type="number" value="1" class="cart-quantity">
+        </div>
+        <i class="bx bx-trash cart-remove"></i>
+    </div>
+        `
+      }
+
+
 
 
 
 //Update total
-function updatetotal() {
-    var cartContent = document.getElementsByClassName('cart-content')[0];
-    var cartBoxes = cartContent.getElementsByClassName('cart-box');
-    var total = 0;
-    for (var i = 0; i < cartBoxes.length; i++) {
-        var cartBox = cartBoxes[i];
-        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
-        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
-        var price = parseFloat(priceElement.innerText.replace('$', ''));
-        var quantity = quantityElement.value;
-        total = total + price * quantity;
+function updateTotal() {
+    const cartContent = document.querySelectorAll('.cart-box');
+    const totalValue = document.querySelector('.total-price');
+
+    let total = 0;
+
+
+    cartContent.forEach(product =>{
+        let priceElement = document.querySelector('.cart-price');
+        let price = parseFloat(priceElement.innerHTML.replace('$', ''))
+        let quantity = product.querySelector('.cart-quantity').value;
+        total += (price * quantity);
+        product.querySelector('.cart-price').innertext = '$' + (price * quantity);
+    });
+
+    totalValue.innerHTML = '$' + total;
+
+        
+    
+        // Add Product Count in Cart Icon
+
+        const cartCount=document.querySelector('.box');
+        let count=itemList.length || myCart.length;
+        cartCount.innerHTML=count;
+
+        if(count==0){
+        cartCount.style.display='none';
+        }else{
+        cartCount.style.display='block';
         }
 
-        // rounding decimal prices
-        total = Math.round(total * 100) / 100;
-
-        document.getElementsByClassName('total-price')[0].innerText = '$' + total;
-    
 }
+
+
+
+//Button with Increment and Decrement Number
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const num = document.querySelector('.num');
+
+
+let a = 1;
+
+plus.addEventListener('click', ()=>{
+    a++;
+    a = (a < 10) ? '' + a : a;
+    num.innerHTML = a;
+});
+
+minus.addEventListener('click', ()=>{
+    if(a > 1) {
+        a--;
+        a = (a < 10) ? '' + a : a;
+        num.innerHTML = a;
+    }
+});
+
+
+// Navbar indicator and active menu icon
+const navBar = document.querySelector('.navbar');
+const plot = document.querySelectorAll('.grp')
+const indi = document.querySelector('.indi');
+
+plot.forEach((grp, index) => {
+    grp.addEventListener('click', e =>{
+        e.preventDefault();// Prevent submitting
+        navBar.querySelector('.active').classList.remove('active');
+        grp.classList.add('active');
+
+        indi.style.transform = `translateX(calc(${index * 123}px))`;
+    });
+});
